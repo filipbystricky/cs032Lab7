f5a09b3e96d762a73a2cf5f46b8eba90660b2c60
Filip Bystricky <filip_bystricky@brown.edu>
2015-04-08 20:52:42 -0400

    implemented bubble sort

--- lab7/Sorting/src/Sorting/BubbleSort.java
+++ lab7/Sorting/src/Sorting/BubbleSort.java
@@ -8,7 +8,28 @@ public class BubbleSort {
 	 */
 	public static int[] bubbleSort(int[] unsortedArray) {
 		//TODO: S1 implements bubble sort
-		
-		return null;
+		boolean isSorted = false;
+
+		while (!isSorted) {
+			isSorted = true;
+			for (int i = 0; i < unsortedArray.length - 1; i++) {
+				if(unsortedArray[i + 1] < unsortedArray[i]) {
+					swap(unsortedArray, i, i + 1);
+					isSorted = false;
+				}
+			}
+		}
+
+		for (int i = 0; i < unsortedArray.length - 1; i++) {
+			assert unsortedArray[i] <= unsortedArray[i + 1];
+		}
+
+		return unsortedArray;
+	}
+
+	private static void swap(int[] arr, int i1, int i2) {
+		int temp = arr[i1];
+		arr[i1] = arr[i2];
+		arr[i2] = temp;
 	}
 }
